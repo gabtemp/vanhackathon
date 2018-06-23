28c7a20822cb22a76ac746e329feba1f08c3db6b
Gabriel Temporim Carneiro <temporim@br.ibm.com>
2018-06-23 16:19:00 -0300

    Creating API and tests for PUT and DELETE methods and adding Javadocs

--- src/main/java/gabtemp/vanhackathon/resource/RestaurantResource.java
+++ src/main/java/gabtemp/vanhackathon/resource/RestaurantResource.java
@@ -13,9 +13,11 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import javax.annotation.Resource;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -30,6 +32,9 @@ import org.springframework.stereotype.Component;
 
 import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
 
+/**
+ * Service for CRUD operations on Restaurant resource
+ */
 @Component
 @Path("/restaurant")
 public class RestaurantResource {
@@ -39,6 +44,11 @@ public class RestaurantResource {
     @Resource
     private RestaurantRepository repository;
 
+    /**
+     * Finds all registered restaurants
+     *
+     * @return a response with the list of all restaurants. Produces a 200 HTTP status code.
+     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response findAll() {
@@ -48,6 +58,12 @@ public class RestaurantResource {
         return Response.ok(all).build();
     }
 
+    /**
+     * Find a registered restaurant by the provided id
+     *
+     * @param id the id of the restaurant
+     * @return a response with the restaurant and a HTTP status 200 if it exists. Returns a 404 otherwise
+     */
     @GET
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
@@ -58,19 +74,31 @@ public class RestaurantResource {
             LOG.info("Registered restaurant with id " + id + " found.");
             return Response.ok(restaurant.get()).build();
         } else {
-            LOG.info("Registered restaurant with id " + id + " not found.");
-            return Response.status(Response.Status.NOT_FOUND).build();
+            String message = "Registered restaurant with id " + id + " not found.";
+            LOG.info(message);
+            return Response.status(Response.Status.NOT_FOUND).entity(message).build();
         }
     }
 
+    /**
+     * Register a new restaurant using the provided fields.
+     *
+     * @param name    the name of the restaurant (required)
+     * @param address the address of the restaurant (required)
+     * @param pickUpDays    the week days the restaurant is available for leftovers pick-up. if empty the restaurant is
+     *                available all days of the week (optional)
+     * @param time    the time of day the restaurant is available for leftovers pick-up (required)
+     * @return a response with the restaurant and a HTTP status 201 if the restaurant is successfully created with a location
+     * header pointing to the newly created resource. Returns a 400 if the mandatory parameters are not provided
+     */
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     public Response create(@FormParam("name") String name, @FormParam("address") String address,
-            @FormParam("pickUpDays") List<String> days, @FormParam("pickUpTime") String time) {
+            @FormParam("pickUpDays") List<String> pickUpDays, @FormParam("pickUpTime") String time) {
         LOG.info("Registering new restaurant with the following parameters: " +
                 "Name=" + name + ", Address=" + address +
-                ", PickUpDays=" + days + ", PickUpTime=" + time);
+                ", PickUpDays=" + pickUpDays + ", PickUpTime=" + time);
 
         if (name == null) {
             LOG.warn("Failed to register a new restaurant: 'Form parameter 'name' is mandatory'");
@@ -88,11 +116,11 @@ public class RestaurantResource {
         }
 
         Set<DayOfWeek> resolvedDays;
-        if (days == null || days.isEmpty()) {
+        if (pickUpDays == null || pickUpDays.isEmpty()) {
             LOG.info("Form parameter 'pickUpDays' not provided. Using all values.");
             resolvedDays = new HashSet<>(Arrays.asList(DayOfWeek.values()));
         } else {
-            resolvedDays = days.stream().map(DayOfWeek::valueOf).collect(Collectors.toSet());
+            resolvedDays = pickUpDays.stream().map(DayOfWeek::valueOf).collect(Collectors.toSet());
         }
 
         Restaurant restaurant = new Restaurant();
@@ -111,5 +139,88 @@ public class RestaurantResource {
         Restaurant saved = repository.save(restaurant);
         LOG.info("New restaurant registered with id " + saved.getId());
         return Response.created(URI.create("/restaurant/" + saved.getId())).entity(saved).build();
+    }
+
+    /**
+     * Updates a registered restaurant with the provided information. Only the non-null fields are updated
+     *
+     * @param id      the id of the restaurant to update
+     * @param name    the name of the restaurant (required)
+     * @param address the address of the restaurant (required)
+     * @param pickUpDays    the week days the restaurant is available for leftovers pick-up. if empty the restaurant is
+     *                available all days of the week (optional)
+     * @param time    the time of day the restaurant is available for leftovers pick-up (required)
+     * @return a response with the restaurant and a HTTP status 200 if the restaurant is successfully updated with the new information.
+     * Returns a 404 HTTP status if the restaurant is not found
+     */
+    @PUT
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response update(@PathParam("id") Long id, @FormParam("name") String name,
+            @FormParam("address") String address, @FormParam("pickUpDays") List<String> pickUpDays,
+            @FormParam("pickUpTime") String time) {
+
+        LOG.info("Updating existing restaurant with the following parameters: " +
+                "Name=" + name + ", Address=" + address +
+                ", PickUpDays=" + pickUpDays + ", PickUpTime=" + time);
+
+        Optional<Restaurant> restaurant = repository.findById(id);
+        if (!restaurant.isPresent()) {
+            String message = "Registered restaurant with id " + id + " not found.";
+            LOG.info(message);
+            return Response.status(Response.Status.NOT_FOUND).entity(message).build();
+        }
+
+        Restaurant updated = restaurant.get();
+        if (name != null) {
+            updated.setName(name);
+        }
+
+        if (address != null) {
+            updated.setAddress(address);
+        }
+
+        if (pickUpDays != null && !pickUpDays.isEmpty()) {
+            Set<DayOfWeek> resolvedDays = pickUpDays.stream().map(DayOfWeek::valueOf).collect(Collectors.toSet());
+            updated.setAvailablePickUpDays(resolvedDays);
+        }
+
+        if (time != null) {
+            try {
+                updated.setPickUpTime(LocalTime.parse(time, DateTimeFormatter.ISO_TIME));
+            } catch (DateTimeParseException e) {
+                String message = "Invalid time format (" + time + ") for the 'pickUpTime' field. The allowed format is 'hh:mm'," +
+                        " where 'hh' is the hour from 0 to 23 and 'mm' are the minutes from 0 to 59.";
+                LOG.error(message, e);
+                return Response.status(BAD_REQUEST).entity(message).build();
+            }
+
+        }
+
+        Restaurant saved = repository.save(updated);
+        return Response.ok(saved).build();
+    }
+
+    /**
+     * Deletes a registered restaurant with the provided id
+     *
+     * @param id the id of the restaurant
+     * @return a response with the deleted restaurant and a HTTP status 200 if the restaurant is successfully deleted.
+     * Returns a 404 HTTP status if the restaurant is not found
+     */
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response delete(@PathParam("id") Long id) {
+        LOG.info("Deleting registered restaurant with id " + id + ".");
+        Optional<Restaurant> restaurant = repository.findById(id);
+        if (!restaurant.isPresent()) {
+            String message = "Registered restaurant with id " + id + " not found.";
+            LOG.info(message);
+            return Response.status(Response.Status.NOT_FOUND).entity(message).build();
+        }
+        repository.delete(restaurant.get());
+        LOG.info("Registered restaurant with id " + id + " deleted.");
+        return Response.ok(restaurant.get()).build();
     }
 }
--- src/test/java/gabtemp/vanhackathon/resource/RestaurantResourceTest.java
+++ src/test/java/gabtemp/vanhackathon/resource/RestaurantResourceTest.java
@@ -1,7 +1,7 @@
 package gabtemp.vanhackathon.resource;
 
 import java.time.DayOfWeek;
-import java.util.Arrays;
+import java.time.LocalTime;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Optional;
@@ -9,13 +9,14 @@ import javax.ws.rs.core.Response;
 
 import gabtemp.vanhackathon.domain.Restaurant;
 import gabtemp.vanhackathon.repository.RestaurantRepository;
-import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static java.time.DayOfWeek.MONDAY;
+import static java.time.DayOfWeek.SUNDAY;
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
@@ -58,7 +59,8 @@ public class RestaurantResourceTest {
 
         Response response = resource.findById(14L);
         assertThat("GET by ID Not Found - response code: ", response.getStatus(), is(404));
-        assertThat("GET by ID Not Found - response entity: ", response.getEntity(), CoreMatchers.nullValue());
+        assertThat("GET by ID Not Found - response entity: ", response.getEntity(),
+                is("Registered restaurant with id 14 not found."));
     }
 
     @Test
@@ -108,13 +110,13 @@ public class RestaurantResourceTest {
         Restaurant expected = new Restaurant();
         expected.setId(1L);
 
-        Response response = resource.create("name", "address", singletonList("FRIDAY"), "23:15");
+        Response response = resource.create("name", "address", asList("FRIDAY", "SUNDAY"), "23:15");
         assertThat("POST successful - response code: ", response.getStatus(), is(201));
         assertThat("POST successful - response entity: ", response.getEntity(), is(expected));
     }
 
     @Test
-    public void testCreateDeafultDay() {
+    public void testCreateDefaultDay() {
         when(repository.save(any(Restaurant.class))).thenAnswer(invocationOnMock -> {
             Restaurant argument = invocationOnMock.getArgument(0);
             argument.setId(1L);
@@ -123,12 +125,71 @@ public class RestaurantResourceTest {
 
         Restaurant expected = new Restaurant();
         expected.setId(1L);
-        expected.setAvailablePickUpDays(new HashSet<>(Arrays.asList(DayOfWeek.values())));
+        expected.setAvailablePickUpDays(new HashSet<>(asList(DayOfWeek.values())));
 
         Response response = resource.create("name", "address", null, "23:15");
         assertThat("POST successful - response code: ", response.getStatus(), is(201));
         assertThat("POST successful - response entity: ", response.getEntity(), is(expected));
         assertThat("POST successful - response entity: ", ((Restaurant) response.getEntity()).getAvailablePickUpDays(),
                 is(expected.getAvailablePickUpDays()));
+    }
+
+    @Test
+    public void testUpdateSuccessful() {
+        when(repository.findById(35L)).thenReturn(Optional.of(new Restaurant()));
+        when(repository.save(any(Restaurant.class))).thenAnswer(
+                invocationOnMock -> invocationOnMock.<Restaurant>getArgument(0));
+
+        Response response = resource.update(35L, "New Address", "New Address", asList("SUNDAY", "MONDAY"), "22:15");
+        Restaurant updated = (Restaurant) response.getEntity();
+
+        assertThat("PUT successful - response code: ", response.getStatus(), is(200));
+        assertThat("PUT successful - name field: ", updated.getName(), is("New Address"));
+        assertThat("PUT successful - address field: ", updated.getAddress(), is("New Address"));
+        assertThat("PUT successful - pickUpDays field: ", updated.getAvailablePickUpDays(), is(new HashSet<>(asList(SUNDAY, MONDAY))));
+        assertThat("PUT successful - pickUpTime field: ", updated.getPickUpTime(), is(LocalTime.of(22, 15)));
+    }
+
+    @Test
+    public void testUpdateNotFound() {
+        when(repository.findById(14L)).thenReturn(Optional.empty());
+
+        Response response = resource.update(14L, null, null, null, null);
+        assertThat("PUT Not Found - response code: ", response.getStatus(), is(404));
+        assertThat("PUT Not Found - response entity: ", response.getEntity(),
+                is("Registered restaurant with id 14 not found."));
+
+    }
+
+    @Test
+    public void testUpdateInvalid() {
+        when(repository.findById(14L)).thenReturn(Optional.of(new Restaurant()));
+
+        Response response = resource.update(14L, null, null, null, "24:67");
+        assertThat("PUT pickUpTime invalid - response code: ", response.getStatus(), is(BAD_REQUEST.getStatusCode()));
+        assertThat("PUT pickUpTime invalid - response entity: ", response.getEntity(),
+                is("Invalid time format (24:67) for the 'pickUpTime' field. The allowed format is 'hh:mm', " +
+                        "where 'hh' is the hour from 0 to 23 and 'mm' are the minutes from 0 to 59."));
+    }
+
+    @Test
+    public void testDeleteNotFound() {
+        when(repository.findById(14L)).thenReturn(Optional.empty());
+
+        Response response = resource.delete(14L);
+        assertThat("DELETE Not Found - response code: ", response.getStatus(), is(404));
+        assertThat("DELETE Not Found - response entity: ", response.getEntity(),
+                is("Registered restaurant with id 14 not found."));
+    }
+
+    @Test
+    public void testDelete() {
+        Restaurant restaurant = new Restaurant();
+
+        when(repository.findById(32L)).thenReturn(Optional.of(restaurant));
+
+        Response response = resource.delete(32L);
+        assertThat("GET by ID - response code: ", response.getStatus(), is(200));
+        assertThat("GET by ID - response entity: ", response.getEntity(), is(restaurant));
     }
 }
