d79a66d6a42e37a2e949c6a2c15e43f6109e8aaf
Gabriel Temporim Carneiro <temporim@br.ibm.com>
2018-06-23 13:38:08 -0300

    Adding log to API calls

--- src/main/java/gabtemp/vanhackathon/resource/RestaurantResource.java
+++ src/main/java/gabtemp/vanhackathon/resource/RestaurantResource.java
@@ -17,13 +17,17 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.google.common.collect.Iterables;
 import gabtemp.vanhackathon.domain.Restaurant;
 import gabtemp.vanhackathon.repository.RestaurantRepository;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 @Component
 @Path("/restaurant")
 public class RestaurantResource {
+
+    private final Logger LOG = Logger.getLogger(RestaurantResource.class);
 
     @Resource
     private RestaurantRepository repository;
@@ -31,18 +35,23 @@ public class RestaurantResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response findAll() {
+        LOG.info("Finding all registered restaurants.");
         Iterable<Restaurant> all = repository.findAll();
+        LOG.info(Iterables.size(all) + " registered restaurants found.");
         return Response.ok(all).build();
     }
 
     @GET
-    @Path("/{id")
+    @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response findById(@PathParam("id") Long id) {
+        LOG.info("Finding registered restaurant with id " + id + ".");
         Optional<Restaurant> restaurant = repository.findById(id);
         if (restaurant.isPresent()) {
+            LOG.info("Registered restaurant with id " + id + " found.");
             return Response.ok(restaurant.get()).build();
         } else {
+            LOG.info("Registered restaurant with id " + id + " not found.");
             return Response.status(Response.Status.NOT_FOUND).build();
         }
     }
@@ -52,6 +61,11 @@ public class RestaurantResource {
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     public Response create(@FormParam("name") String name, @FormParam("address") String address,
             @FormParam("day") String day, @FormParam("time") String time) {
+        LOG.info("Registering new restaurant with the following parameters:" +
+                "Name=" + name +
+                "Address=" + address +
+                "PickUpDays=" + day +
+                "PickUpTime=" + time);
 
         Restaurant restaurant = new Restaurant();
         restaurant.setName(name);
@@ -60,7 +74,7 @@ public class RestaurantResource {
         restaurant.setPickUpTime(LocalTime.parse(time, DateTimeFormatter.ISO_TIME));
 
         Restaurant saved = repository.save(restaurant);
-
+        LOG.info("New restaurant registered with id " + saved.getId());
         return Response.created(URI.create("/restaurant/" + saved.getId())).entity(saved).build();
     }
 }
