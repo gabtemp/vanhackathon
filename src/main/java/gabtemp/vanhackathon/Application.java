20f307ac908e29b051d85ff2693ea50ca17775bd
Gabriel Temporim Carneiro <temporim@br.ibm.com>
2018-06-23 12:52:38 -0300

    Adding Spring Boot Application

---
+++ src/main/java/gabtemp/vanhackathon/Application.java
@@ -0,0 +1,16 @@
+package gabtemp.vanhackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main application class
+ */
+@SpringBootApplication
+@EnableAutoConfiguration
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
